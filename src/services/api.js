62f7ab87281c38b6b36480d285d8fbccb53281f4
unknown <bruno_correa_ds@hotmail.com>
2022-12-09 20:04:40 -0300

    ajuste

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-//https://sujeitoprogramador.com/r-api/?api=filmes
+https://sujeitoprogramador.com/r-api/?api=filmes
 
 const api = axios.create({
      baseURL:'https://sujeitoprogramador.com/'
